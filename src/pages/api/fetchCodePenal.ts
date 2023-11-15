896327b87cdc7f62ecbb186ab69612c3714ea4f5
Nicolas Hedoire <nicolas.hedoire@seckiot.fr>
2023-11-15 20:21:24 +0100

    add improvment on fetchpenalcode

--- src/pages/api/fetchCodePenal.ts
+++ src/pages/api/fetchCodePenal.ts
@@ -2,34 +2,62 @@ import { Article } from "@/types/article";
 import { JSDOM } from "jsdom";
 import { NextApiRequest, NextApiResponse } from "next";
 
+const fetchWithTimeout = async (resource: string, options: { timeout: number}) => {
+  const { timeout = 8000 } = options;
+
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal  
+  });
+  clearTimeout(id);
+
+  return response;
+};
+
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const response = await fetch(
-    "https://www.legifrance.gouv.fr/codes/section_lc/LEGITEXT000006070719/LEGISCTA000006089684/",
-  );
-  const html = await response.text();
-  const dom = new JSDOM(html);
-  const document = dom.window.document;
-  let articles: Article[] = [];
 
-  document
-    .querySelectorAll(".js-child.list-article-consommation")
-    .forEach((articleElement) => {
-      const titleElement: Element | null = articleElement.querySelector(".name-article");
-      const title: string = titleElement?.textContent?.trim() ?? "";
-      const url: string = titleElement?.querySelector("a")?.href ?? "";
-      const contentElement: Element | null = articleElement.querySelector(".content");
-      let content = "";
-      if (contentElement) {
-        contentElement.querySelectorAll("p").forEach((p) => {
-          content += p.textContent + "\n";
-        });
-      }
+  try {
+    const response = await fetchWithTimeout(
+      "https://www.legifrance.gouv.fr/codes/section_lc/LEGITEXT000006070719/LEGISCTA000006089684/",
+      { timeout: 10000 }
+    );
 
-      articles.push({ title, url, content: `<p>${content.trim()}</p>` });
-    });
+    if (!response.ok) {
+      throw new Error(`Erreur HTTP: ${response.status}`);
+    }
 
-  res.status(200).json({ data: articles });
+    const html = await response.text();
+    const dom = new JSDOM(html);
+    const document = dom.window.document;
+    let articles: Article[] = [];
+  
+    document
+      .querySelectorAll(".js-child.list-article-consommation")
+      .forEach((articleElement) => {
+        const titleElement: Element | null = articleElement.querySelector(".name-article");
+        const title: string = titleElement?.textContent?.trim() ?? "";
+        const url: string = titleElement?.querySelector("a")?.href ?? "";
+        const contentElement: Element | null = articleElement.querySelector(".content");
+        let content = "";
+        if (contentElement) {
+          contentElement.querySelectorAll("p").forEach((p) => {
+            content += p.textContent + "\n";
+          });
+        }
+  
+        articles.push({ title, url, content: `<p>${content.trim()}</p>` });
+      });
+  
+    res.status(200).json({ data: articles });
+  } catch (error) {
+    console.error("Erreur lors de la requête:", error);
+    res.status(500).json({ error: "Erreur lors de la récupération des données." });
+  }
 }
